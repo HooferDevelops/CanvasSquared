b4943bfab569c92eec67ac96a3ad69bb335ad6aa
HooferDevelops <BatHun0214A@mischools.org>
2022-02-01 20:12:53 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("I am index.js");
+/*  Made for Canvas Squared
+    (https://github.com/HooferDevelops/CanvasSquared)
+    ---------------------------------------------------
+    Module written by:
+    Hoofer
+    ---------------------------------------------------
+    Creative Commons Zero v1.0 Universal
+    (https://creativecommons.org/publicdomain/zero/1.0/)
+*/
+
+
+// Main Module File for the source.
+// Use me to initialize a new canvas!
